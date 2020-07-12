ed175d2e4ec783f6baba6ea9bb1c96b41b7e36c2
Theo Lepage <theo.lepage@epita.fr>
2020-07-13 00:06:56 +0200

    fix(numpy_convert): fix bug by making from_buffer determine strides

--- src/numpy_convert.hxx
+++ src/numpy_convert.hxx
@@ -8,9 +8,6 @@ mln::ndbuffer_image numpy_to_ndbuffer(py::array_t<T> array)
 {
     py::buffer_info info = array.request();
 
-    // FIXME: For some reason omitting this line causes a segfault.
-    py::format_descriptor<T>::format();
-
     int ndim = info.ndim;
 
     // Determine ndbuffer_image shape
@@ -18,16 +15,11 @@ mln::ndbuffer_image numpy_to_ndbuffer(py::array_t<T> array)
     for (auto i = 0; i < ndim; i++)
         shape[i] = info.shape[i];
 
-    // Determine ndbuffer_image strides
-    std::ptrdiff_t byte_strides[] = { 0 };
-    for (auto i = 0; i < ndim; i++)
-        byte_strides[i] = info.strides[ndim - 1 - i];
-
     return mln::ndbuffer_image::from_buffer(static_cast<std::byte*>(info.ptr),
-                                            mln::sample_type_id::DOUBLE,
+                                            mln::sample_type_id::INT8,
                                             ndim,
                                             shape,
-                                            byte_strides,
+                                            nullptr,
                                             false);
 }
 
@@ -37,14 +29,14 @@ py::array_t<T> ndbuffer_to_numpy(mln::ndbuffer_image buffer)
     int ndim = buffer.pdim();
 
     // Determine numpy array shape
-    std::vector<ssize_t> shape;
+    std::vector<ssize_t> shape(ndim);
     for (auto i = 0; i < ndim; i++)
-        shape.push_back(buffer.size(i));
+        shape[i] = buffer.size(i);
 
-    // Determine numpy array strides
-    std::vector<ssize_t> strides;
-    for (auto i = ndim - 1; i >= 0; i--)
-        strides.push_back(buffer.byte_stride(i));
+    // Determine numpy array strides (reverse order)
+    std::vector<ssize_t> strides(ndim);
+    for (auto i = 0; i < ndim; i++)
+        strides[i] = buffer.byte_stride(ndim - 1 - i);
 
     auto info = py::buffer_info(
         buffer.buffer(),
