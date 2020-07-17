b06ba04fea0fb7a061d983018d9b1443259400cd
Theo Lepage <theo.lepage@epita.fr>
2020-07-17 22:33:16 +0200

    fix type of images loaded

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,18 +5,31 @@ import matplotlib.pyplot as plt
 from skimage import data
 
 def mse(imgA, imgB):
-	err = np.sum((imgA.astype("float") - imgB.astype("float")) ** 2)
-	err /= float(imgA.shape[0] * imgA.shape[1])
-	return err
+    err = np.sum((imgA.astype("float") - imgB.astype("float")) ** 2)
+    err /= float(imgA.shape[0] * imgA.shape[1])
+    print(err)
+    return err
+
+lena = plt.imread('lena.png') * 255
+lena = lena.astype(np.uint8)
+A_dilation_disc5 = plt.imread('lena_dilation_disc5.png') * 255
+A_dilation_rect20 = plt.imread('lena_dilation_rect20-20.png') * 255
+
+B_dilation_disc5 = pln.morpho.dilation(lena, pln.se.disc(5))
+B_dilation_rect20 = pln.morpho.dilation(lena, pln.se.rectangle(width=20, height=20))
+
+print(B_dilation_disc5)
+plt.imshow(A_dilation_disc5.astype(np.uint8))
+plt.show()
+
+plt.imshow(B_dilation_disc5)
+plt.show()
 
 class TestDilation(unittest.TestCase):
-
-    lena = plt.imread('lena.png') * 255
-    A_dilation_disc5 = plt.imread('lena_dilation_disc5.png') * 255
-    A_dilation_rect20 = plt.imread('lena_dilation_rect20-20.png') * 255
-
-    B_dilation_disc5 = pln.morpho.dilation(lena, pln.se.disc(5))
-    B_dilation_rect20 = pln.morpho.dilation(lena, pln.se.rectangle(width=20, height=20))
+    # def test_lena_disc_t(test):
+    #     ref = 
+    #     out = 
+    #     compare
 
     def test_shape(self):
         self.assertEqual(lena.shape, A_dilation_disc5.shape)
