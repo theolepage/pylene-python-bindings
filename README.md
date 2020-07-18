893629b1745651c20dab99c4e57f683009b16ca3
Theo Lepage <theo.lepage@epita.fr>
2020-07-18 20:31:13 +0200

    fix(tests): add title with error

--- README.md
+++ README.md
@@ -17,14 +17,14 @@ out1 = pln.morpho.dilation(input1, pln.se.disc(4))
 out2 = pln.morpho.dilation(input2, pln.se.rectangle(width=11, height=5))
 ```
 
-Alternatively, you can run our test suite by typing `make test` in the build directory.
-
 ## Build
 
 1. Create the build directory at the root of the repository: `mkdir build && cd build` (for releases) or `mkdir build_debug && cd build_debug`
 2. `conan remote add lrde-public https://artifactory.lrde.epita.fr/artifactory/api/conan/lrde-public`
 3. `conan install .. --build missing -s compiler.cppstd=20`
 4.  `cmake ..; make`
+
+Alternatively, you can run our test suite by typing `make test` in the build directory.
 
 ## Authors
 
--- tests/test_example.py
+++ tests/test_example.py
@@ -4,92 +4,118 @@ import unittest
 import matplotlib.pyplot as plt
 from skimage import data
 
+ALWAYS_SHOW_IMAGES = False
+
 def open_image(image_path):
     image = plt.imread(image_path) * 255
     return image.astype(np.uint8)
 
-def mse(imgA, imgB):
-    err = np.sum((imgA.astype("float") - imgB.astype("float")) ** 2)
-    err /= float(imgA.shape[0] * imgA.shape[1])
+def mse(image_a, image_b):
+    err = np.sum((image_a.astype("float") - image_b.astype("float")) ** 2)
+    err /= float(image_a.shape[0] * image_a.shape[1])
     return err
-
-def are_same(out, ref, debug=False):
-    if not np.array_equal(out.shape, ref.shape):
-        print("Shape Error: out image does not have the same shape as ref")
-        return False
-    mse_res = mse(out, ref)
-
-    if debug:
-        print(f"MSE : {mse_res}")
-
-        f = plt.figure()
-        actual_plot = f.add_subplot(1, 2, 2)
-        plt.imshow(out)
-        actual_plot.set_xlabel('Actual')
-        ref_plot = f.add_subplot(1, 2, 1)
-        plt.imshow(ref)
-        ref_plot.set_xlabel('Expected')
-        plt.show(block=True)
-
-    return mse_res == 0
 
 lena = open_image("lena.png")
 lena_gray = open_image("lena_gray.png")
 
-class TestDilation(unittest.TestCase):
+class ImagesEqualsAssertions:
+    def assertImagesEquals(self, actual, expected, negative=False):
+        if not np.array_equal(actual.shape, expected.shape):
+            raise AssertionError("Shape error: out image does not have the same shape as ref")
+
+        if not actual.dtype == expected.dtype:
+            raise AssertionError("Type error: out image does not have the same type as ref")
+
+        mse_res = mse(actual, expected)
+
+        if ALWAYS_SHOW_IMAGES or (not negative and not mse_res == 0):
+            f = plt.figure()
+            plt.box(False)
+
+            # Title
+            fig = plt.text(0, 0, self.id() + "\n" + 'Error: {}'.format(mse_res), fontsize=12)
+            fig.axes.get_xaxis().set_visible(False)
+            fig.axes.get_yaxis().set_visible(False)
+
+            # Show output image
+            actual_plot = f.add_subplot(1, 2, 1)
+            plt.imshow(actual)
+            actual_plot.set_xlabel('Actual')
+            actual_plot.axes.xaxis.set_label_position('top')
+
+            # Show expected image
+            ref_plot = f.add_subplot(1, 2, 2)
+            plt.imshow(expected)
+            ref_plot.set_xlabel('Expected')
+            ref_plot.axes.xaxis.set_label_position('top')
+
+            plt.show(block=True)
+
+        if not mse_res == 0:
+            raise AssertionError("Output image is different from ref (error: {})".format(mse_res))
+
+    def assertImagesNotEquals(self, actual, expected):
+        try:
+            self.assertImagesEquals(actual, expected, True)
+        except AssertionError:
+            pass
+        else:
+            raise AssertionError("Output image is equal to ref")
+
+class TestDilation(unittest.TestCase, ImagesEqualsAssertions):
 
     def test_lena_disc5(self):
         ref_dilation_disc5 = open_image('lena_dilation_disc5.png')
         out_dilation_disc5 = pln.morpho.dilation(lena, pln.se.disc(5))
-        self.assertTrue(are_same(out_dilation_disc5, ref_dilation_disc5))
+        self.assertImagesEquals(out_dilation_disc5, ref_dilation_disc5)
 
     def test_lena_rect20(self):
         ref_dilation_rect20 = open_image('lena_dilation_rect20-20.png')
         out_dilation_rect20 = pln.morpho.dilation(lena, pln.se.rectangle(width=20, height=20))
-        self.assertTrue(are_same(out_dilation_rect20, ref_dilation_rect20))
+        self.assertImagesEquals(out_dilation_rect20, ref_dilation_rect20)
 
-class TestErosion(unittest.TestCase):
+class TestErosion(unittest.TestCase, ImagesEqualsAssertions):
 
     def test_lena_disc19(self):
         ref_erosion_disc19 = open_image('lena_erosion_disc19.png')
         out_erosion_disc19 = pln.morpho.erosion(lena, pln.se.disc(19))
-        self.assertTrue(are_same(out_erosion_disc19, ref_erosion_disc19))
+        self.assertImagesEquals(out_erosion_disc19, ref_erosion_disc19)
 
     def test_lena_rect1510(self):
         ref_erosion_rect1510 = open_image('lena_erosion_rect15-10.png')
         out_erosion_rect1510 = pln.morpho.erosion(lena, pln.se.rectangle(width=15, height=10))
-        self.assertTrue(are_same(out_erosion_rect1510, ref_erosion_rect1510))
+        self.assertImagesEquals(out_erosion_rect1510, ref_erosion_rect1510)
 
-class TestClosing(unittest.TestCase):
+class TestClosing(unittest.TestCase, ImagesEqualsAssertions):
 
     def test_lena_gray_disc2(self):
         ref_closing_disc2 = open_image('lena_gray_closing_disc2.png')
         out_closing_disc2 = pln.morpho.closing(lena_gray, pln.se.disc(2))
-        self.assertTrue(are_same(out_closing_disc2, ref_closing_disc2))
+        self.assertImagesEquals(out_closing_disc2, ref_closing_disc2)
 
     def test_lena_gray_rect0531(self):
         ref_closing_rect0531 = open_image('lena_gray_closing_rect5-31.png')
         out_closing_rect0531 = pln.morpho.closing(lena_gray, pln.se.rectangle(width=5, height=31))
-        self.assertTrue(are_same(out_closing_rect0531, ref_closing_rect0531))
+        self.assertImagesEquals(out_closing_rect0531, ref_closing_rect0531)
 
-class Testopening(unittest.TestCase):
+class TestOpening(unittest.TestCase, ImagesEqualsAssertions):
 
     def test_lena_disc10(self):
         ref_opening_disc10 = open_image('lena_opening_disc10.png')
         out_opening_disc10 = pln.morpho.opening(lena, pln.se.disc(10))
-        self.assertTrue(are_same(out_opening_disc10, ref_opening_disc10))
+        self.assertImagesEquals(out_opening_disc10, ref_opening_disc10)
 
     def test_lena_rect0519(self):
         ref_opening_rect0519 = open_image('lena_opening_rect5-19.png')
         out_opening_rect0519 = pln.morpho.opening(lena, pln.se.rectangle(width=5, height=19))
-        self.assertTrue(are_same(out_opening_rect0519, ref_opening_rect0519))
+        self.assertImagesEquals(out_opening_rect0519, ref_opening_rect0519)
 
-class TestMSE(unittest.TestCase):
+class TestMSE(unittest.TestCase, ImagesEqualsAssertions):
 
     def test_lena_disc10vsdisc11(self):
         ref_opening_disc10 = open_image('lena_opening_disc10.png')
         out_opening_disc11 = pln.morpho.opening(lena, pln.se.disc(11))
-        self.assertFalse(are_same(out_opening_disc11, ref_opening_disc10, False))
+        self.assertImagesNotEquals(out_opening_disc11, ref_opening_disc10)
 
 if __name__ == '__main__':
     unittest.main()
